3b927be68e522a9ad6e7695c9bf6c5c369237aa9
chibaobao123 <baobaochi631999@gmail.com>
2024-11-27 21:22:49 +0700

    step 1: API Hello world

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+import axios from "./util/axios.customizie";
+//custom file thì mai mốt có sửa đường dẫn backend
+//thì chỉ cần vô file axios.customizie
+//để sửa ở phần import.meta.env.VITE_BACKEND_URL
+
+function App() {
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      const res = await axios.get(`/v1/api/`);
+      console.log(res.data);
+    };
+
+    fetchHelloWorld();
+  }, []);
+  return <>hello world !!!</>;
+}
+
+export default App;
