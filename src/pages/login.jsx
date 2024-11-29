ebc3c2cbbf698ef0a276ee828e1729027226e43f
chibaobao123 <baobaochi631999@gmail.com>
2024-11-29 11:49:46 +0700

    Step 5: APi Login with Node,js

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,79 @@
+import { Button, Form, Input, notification } from "antd";
+import { loginUserApi } from "../util/api";
+import { useNavigate } from "react-router";
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    const { email, password } = values;
+
+    const res = await loginUserApi(email, password);
+    console.log("Success:", res);
+
+    if (res) {
+      notification.success({
+        message: "Login user",
+        description: "success",
+      });
+      navigate("/");
+    } else {
+      notification.error({
+        message: "Login user",
+        description: "ERROR",
+      });
+    }
+  };
+
+  return (
+    <div style={{ margin: 50 }}>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        onFinish={onFinish}
+        autoComplete="off"
+        layout="vertical"
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email !",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/util/api.js
+++ src/util/api.js
@@ -12,4 +12,15 @@ const createUserApi = (name, email, password) => {
   return axios.post(URL_API, data);
 };
 
-export { createUserApi };
+const loginUserApi = (email, password) => {
+  const URL_API = `/v1/api/login`;
+
+  const data = {
+    email,
+    password,
+  };
+
+  return axios.post(URL_API, data);
+};
+
+export { createUserApi, loginUserApi };
